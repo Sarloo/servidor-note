7d98e332282fb5468d6905d59ad26f90bd4ddf42
Sarloo <carloscecor05@gmail.com>
2026-01-29 14:43:09 -0600

    Manejo de rutas en node

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const http = require('http');
 
 // definir el puerto y la direccion del servidor
 const PORT = 3000;
+
+// crear el servidor
 const servidor = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hola Mundo\n');
+    // manejo de diferentes rutas
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('pagina principal\n');
+    }
+    else if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('acerca de nosotros\n');
+    }
+    else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('pagina no encontrada\n');
+    }
+    
 });
 
 // iniciar el servidor y escuchar en el puerto definido
